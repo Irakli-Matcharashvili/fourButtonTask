8dd5ad7016ebcba7df0eb790f4b55e1ae155d78c
Irakli-Matcharashvili <96695372+Irakli-Matcharashvili@users.noreply.github.com>
2022-01-06 14:19:22 +0400

    remove unused code for master

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -8,21 +8,6 @@ import android.widget.TextView
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.try_main_fragment)
-        val minusButton: Button = findViewById(R.id.appTryMinusBottom)
-        val plusButton: Button = findViewById(R.id.appTryPlusBottom)
-        val zeroTextView: TextView = findViewById(R.id.appTryZeroText)
-        minusButton.setOnClickListener {
-            var number = zeroTextView.text.toString().toInt()
-            number--
-            zeroTextView.text = "$number"
-        }
-        plusButton.setOnClickListener {
-            var number = zeroTextView.text.toString().toInt()
-            number++
-            zeroTextView.text = "$number"
-
-
-        }
+        setContentView(R.layout.activity_main)
     }
 }
